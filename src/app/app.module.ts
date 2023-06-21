a2db980c8e413afe6463f437fd546d6b7f35f33b
dfurrer <diegodellepianefurrer@gmail.com>
2023-06-21 17:05:45 -0300

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainWrapperComponent } from './main-wrapper/main-wrapper.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    MainWrapperComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
+  ],
+  exports: [
+    MainComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
